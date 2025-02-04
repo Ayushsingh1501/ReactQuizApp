1b9b13be693efbf9848376066c8c0e55c3cf778e
Ayushsingh1501 <147020955+Ayushsingh1501@users.noreply.github.com>
2025-02-04 13:55:56 +0530

    Update Footer.js

--- Reactjs-Quiz-App-master/src/components/Footer/Footer.js
+++ Reactjs-Quiz-App-master/src/components/Footer/Footer.js
@@ -5,14 +5,6 @@ const Footer = () => {
         textAlign: "center",
         marginBottom: 10,
       }}
-    >
-      Made with ♥ by{" "}
-      <a
-        href="https://www.youtube.com/roadsidecoder"
-        style={{ cursor: "pointer" }}
-      >
-        Roadside Coder
-      </a>
     </div>
   );
 };
